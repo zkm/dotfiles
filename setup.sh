140169a041a2151d08210cb0e344e98f616f45bd
Zach Schneider <me@zachschneider.com>
2023-02-07 18:50:39 -0600

    adding zprofile to the setup process

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ rm ~/.aliases ~/.gitconfig ~/.vimrc ~/.vimrc.plugs ~/.zshrc ~/.tmux.conf
 rm -rf ~/.zsh ~/.bin
 
 echo "Installing Homebrew... So sorry..."
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "Using Homebrew to install tmux and neovim..."
@@ -20,13 +20,14 @@ echo "source ~/.vimrc" >> ~/.config/init.vim
 
 echo "Creating symlinks to dotfiles in this directory..."
 ln -s $(pwd)/aliases ~/.aliases
+ln -s $(pwd)/bin ~/.bin
 ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/zsh ~/.zsh
+ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/zprofile ~/.zprofile
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/vimrc.plugs ~/.vimrc.plugs
-ln -s $(pwd)/zsh ~/.zsh
-ln -s $(pwd)/bin ~/.bin
-ln -s $(pwd)/zshrc ~/.zshrc
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
 echo "Installing vim-plug and plugins for neovim..."
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
