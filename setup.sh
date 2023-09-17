a97f846ac93229fe33d383c2836e975bae135daf
Zach Schneider <me@zachschneider.com>
2023-09-16 19:40:14 -0500

    Added linux support

--- setup.sh
+++ setup.sh
@@ -1,36 +1,124 @@
-echo "Setting shell to zsh..."
-chsh -s $(which zsh);
+#/bin/bash
 
-echo "Removing previous dotfiles..."
-rm ~/.aliases ~/.gitconfig ~/.vimrc ~/.vimrc.plugs ~/.zshrc ~/.tmux.conf
-rm -rf ~/.zsh ~/.bin
+function set_zsh() {
+    echo "Setting shell to zsh..."
+    chsh -s $(which zsh);
+}
 
-echo "Installing Homebrew... So sorry..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
+function setup_shell() {
+    case $SHELL in
+        */bash)
+            set_zsh
+        ;;
+    esac
+}
 
-echo "Using Homebrew to install tmux and neovim..."
-brew update
-brew install tmux
-brew install neovim
+function clear_old_dotfiles() {
+    echo "Removing previous dotfiles..."
+    sudo rm ~/.aliases ~/.gitconfig ~/.vimrc ~/.vimrc.plugs ~/.zshrc ~/.tmux.conf
+    sudo rm -rf ~/.zsh ~/.bin
+}
 
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" > ~/.config/init.vim
-echo "let &packpath = &runtimepath" >> ~/.config/init.vim
-echo "source ~/.vimrc" >> ~/.config/init.vim
+function install_homebrew() {
+    echo "Installing Homebrew..."
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+}
 
-echo "Creating symlinks to dotfiles in this directory..."
-ln -s $(pwd)/aliases ~/.aliases
-ln -s $(pwd)/bin ~/.bin
-ln -s $(pwd)/gitconfig ~/.gitconfig
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
-ln -s $(pwd)/zsh ~/.zsh
-ln -s $(pwd)/zshrc ~/.zshrc
-ln -s $(pwd)/p10k.zsh ~/.p10k.zsh
-ln -s $(pwd)/zprofile ~/.zprofile
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/vimrc.plugs ~/.vimrc.plugs
+function install_homebrew_packages() {
+    echo "Installing Homebrew packages..."
+    brew install tmux
+    brew install neovim
+    brew install the_silver_searcher
+    brew install node
+    brew install neofetch
+}
 
-echo "Installing vim-plug and plugins for neovim..."
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.vim/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-vim +PlugInstall +qall;
+install_with_pacman() {
+    echo "Installing packages with Pacman..."
+    sudo pacman -S --needed curl git tmux neovim the_silver_searcher nodejs neofetch
+}
+
+# Function to install packages using APT (Debian/Ubuntu)
+install_with_apt() {
+    echo "Installing packages with APT..."
+    sudo apt-get update
+    sudo apt-get install -y curl git tmux neovim the_silver_searcher nodejs neofetch
+}
+
+# Function to install packages using YUM (RHEL/CentOS)
+install_with_yum() {
+    echo "Installing packages with YUM..."
+    sudo yum install -y curl git tmux neovim the_silver_searcher nodejs neofetch
+}
+
+function setup_neovim() {
+    echo "Initializing the config for neovim..."
+    mkdir -p ~/.config
+    mkdir -p ~/.config/nvim
+    touch ~/.config/nvim/init.vim
+    
+    echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" > ~/.config/nvim/init.vim
+    echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+    echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
+    # echo "lua require('config')" >> ~/.config/nvim/init.vim
+}
+
+function create_dotfiles() {
+    echo "Creating dotfiles..."
+    ln -s $(pwd)/aliases ~/.aliases
+    ln -s $(pwd)/gitconfig ~/.gitconfig
+    ln -s $(pwd)/vimrc ~/.vimrc
+    ln -s $(pwd)/vimrc.plugs ~/.vimrc.plugs
+    ln -s $(pwd)/zsh ~/.zsh
+    ln -s $(pwd)/bin ~/.bin
+    ln -s $(pwd)/zshrc ~/.zshrc
+    ln -s $(pwd)/tmux.conf ~/.tmux.conf
+}
+
+function setup_vim_plug() {
+    echo "Installing neovim-plug..."
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+    nvim --headless +PlugInstall +qall
+}
+
+function install_copilot() {
+    if [[ -d ~/.config/nvim/pack/github/start/copilot.vim ]]; then
+        echo "Installing Github copilot..."
+        git clone https://github.com/github/copilot.vim.git ~/.config/nvim/pack/github/start/copilot.vim
+        nvim +Copilot +setup
+    fi
+}
+
+setup_shell
+clear_old_dotfiles
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    install_homebrew
+    install_homebrew_packages
+else
+    # Determine the Linux distribution and install packages accordingly
+    if [[ -x "$(command -v pacman)" ]]; then
+        # Arch Linux (Pacman)
+        install_with_pacman
+        elif [[ -x "$(command -v apt-get)" ]]; then
+        # Debian/Ubuntu (APT)
+        install_with_apt
+        elif [[ -x "$(command -v yum)" ]]; then
+        # RHEL/CentOS (YUM)
+        install_with_yum
+    else
+        echo "Unsupported Linux distribution. Please install packages manually."
+        exit 1
+    fi
+fi
+
+setup_neovim
+create_dotfiles
+setup_vim_plug
+install_copilot
--- vimrc
+++ vimrc
@@ -1,3 +1,4 @@
+set number
 if filereadable(expand("$HOME/.vimrc.plugs"))
   source $HOME/.vimrc.plugs
 endif
